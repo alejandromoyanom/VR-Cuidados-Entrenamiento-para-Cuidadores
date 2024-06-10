4ae6af57aeba8e1358404639eac8dc182a1029c7
Alejandro Moyano <alejandro.moyano@sansano.usm.cl>
2024-06-10 01:07:51 -0400

    6 - Cambios proyecto

---
+++ Assets/Proyecto/Escenarios/Escenario 1/Scripts/Socket/ObjectVR.cs
@@ -0,0 +1,72 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class ObjectVR : MonoBehaviour
+{
+    public GameObject socket; // Referencia al socket específico asociado al objeto
+    private XRGrabInteractable grabInteractable; // Componente para detectar si está siendo agarrado
+    private bool isNearSocket = false; // Bandera para verificar si el objeto está cerca del socket
+
+    void Start()
+    {
+        // Obtener el componente XRGrabInteractable si está presente
+        grabInteractable = GetComponent<XRGrabInteractable>();
+
+        if (grabInteractable == null)
+        {
+            Debug.LogWarning("No se encontró el componente XRGrabInteractable en este objeto.");
+        }
+        else
+        {
+            // Suscribirse al evento de agarrado
+            grabInteractable.selectEntered.AddListener(OnGrabbed);
+            // Suscribirse al evento de soltado
+            grabInteractable.selectExited.AddListener(OnReleased);
+        }
+    }
+
+    void OnGrabbed(SelectEnterEventArgs args)
+    {
+        
+        // Activar el socket al agarrar el objeto
+        socket.SetActive(true);
+    }
+
+    void OnReleased(SelectExitEventArgs args)
+    {
+
+        if (isNearSocket)
+        {
+            // Mantener el socket activo si está cerca de él al soltar
+            socket.SetActive(true);
+
+            // Posicionar el objeto en el socket
+            transform.position = socket.transform.position;
+            transform.rotation = socket.transform.rotation;
+
+        }
+        else
+        {
+            // Desactivar el socket si no está cerca
+            socket.SetActive(false);
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject == socket)
+        {
+            isNearSocket = true;
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject == socket)
+        {
+            isNearSocket = false;
+        }
+    }
+}
