9207ee3db51a2dd4bed25bcd149b7128af0ae7d1
Alejandro Moyano <alejandro.moyano@sansano.usm.cl>
2024-08-13 23:39:06 -0400

    22 - Actualizacion

---
+++ Assets/Proyecto/Escenarios/ProyectoTitulo/Scripts/AseoConfortScripts/ShoeGrab.cs
@@ -0,0 +1,80 @@
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class ShoeGrab : MonoBehaviour
+{
+    public GameObject socket; // Referencia al socket específico asociado al zapato
+    private XRGrabInteractable grabInteractable; // Componente para detectar si está siendo agarrado
+    private Rigidbody rb;
+    private bool isNearSocket = false; // Bandera para verificar si el zapato está cerca del socket
+
+    void Start()
+    {
+        // Obtener el componente XRGrabInteractable si está presente
+        grabInteractable = GetComponent<XRGrabInteractable>();
+        
+        rb = GetComponent<Rigidbody>();
+
+        if (grabInteractable == null)
+        {
+            Debug.LogWarning("No se encontró el componente XRGrabInteractable en este zapato.");
+        }
+        else
+        {
+            // Suscribirse al evento de agarre
+            grabInteractable.selectEntered.AddListener(OnGrabbed);
+            // Suscribirse al evento de soltado
+            grabInteractable.selectExited.AddListener(OnReleased);
+        }
+    }
+
+    void OnGrabbed(SelectEnterEventArgs args)
+    {
+        // Al agarrar, desbloquear las restricciones para que el zapato pueda moverse
+        rb.constraints = RigidbodyConstraints.None;
+        // Activar el socket al agarrar el zapato
+        socket.SetActive(true);
+    }
+
+    void OnReleased(SelectExitEventArgs args)
+    {
+        if (isNearSocket)
+        {
+            // Obtener el Attach Transform del socket
+            XRSocketInteractor socketInteractor = socket.GetComponent<XRSocketInteractor>();
+            Transform attachTransform = socketInteractor.attachTransform != null ? socketInteractor.attachTransform : socket.transform;
+
+            // Alinear la posición y rotación del zapato con el Attach Transform del socket
+            transform.position = attachTransform.position;
+            transform.rotation = attachTransform.rotation;
+
+
+            // Desactivar el componente XRGrabInteractable para evitar que el zapato se agarre nuevamente
+            grabInteractable.enabled = false;
+
+            // Desactivar el socket
+            socket.SetActive(false);
+        }
+        else
+        {
+            // Desactivar el socket si no está cerca
+            socket.SetActive(false);
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject == socket)
+        {
+            isNearSocket = true;
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject == socket)
+        {
+            isNearSocket = false;
+        }
+    }
+}
